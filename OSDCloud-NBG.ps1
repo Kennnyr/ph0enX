d8ecd7681cab1b7ab37bcc51deac0840e18b70e8
Kennnyr <130184226+Kennnyr@users.noreply.github.com>
2024-11-05 11:36:34 +0100

    Update OSDCloud-NBG.ps1

--- OSDCloud-NBG.ps1
+++ OSDCloud-NBG.ps1
@@ -13,7 +13,7 @@ $OSReleaseID = '23H2' #Used to Determine Driver Pack
 
 #Set OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$False
+    Restart = [bool]$True
     RecoveryPartition = [bool]$true
     OEMActivation = [bool]$True
     WindowsUpdate = [bool]$true
